2c89f81593fcee4f632e037cba66cd3ede7f91e3
Sadness <95174481+unf6@users.noreply.github.com>
2023-11-23 13:41:27 -0800

    Update m.py

--- m.py
+++ m.py
@@ -9,15 +9,6 @@ def get_windows_hwid():
     hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
     return hwid
 
-def get_linux_hwid():
-    try:
-        # Run dmidecode and grep for UUID
-        output = subprocess.check_output(['dmidecode', '-s', 'system-uuid']).decode().strip()
-        return output
-    except subprocess.CalledProcessError:
-        print('[ERROR] Failed to get Linux HWID')
-        return None
-
 def get_mac_hwid():
     try:
         # Run system_profiler and grep for hardware UUID
@@ -40,8 +31,6 @@ site = requests.get('https://luxury-kleicha-549dac.netlify.app/hwid.txt')
 try:
     if platform.system().lower() == 'windows':
         hardwareid = get_windows_hwid()
-    elif platform.system().lower() == 'linux':
-        hardwareid = get_linux_hwid()
     elif platform.system().lower() == 'darwin':
         hardwareid = get_mac_hwid()
     elif platform.system().lower() == 'android':
