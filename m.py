994c10edc82d752f5eebe19675871b7d46365b8b
Sadness <95174481+unf6@users.noreply.github.com>
2023-11-23 14:07:17 -0800

    Update m.py

--- m.py
+++ m.py
@@ -1,39 +1,18 @@
 import subprocess
-import requests
-import time
-import os
-import uuid
-import platform
 
-def get_windows_hwid():
-    hardwareid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-    return hardwareid
+def get_android_device_id():
+    try:
+        # Run the getprop command to retrieve the ro.serialno property
+        result = subprocess.check_output(['getprop', 'ro.serialno']).decode().strip()
+        return result
+    except subprocess.CalledProcessError as e:
+        print(f'Error retrieving device ID: {e}')
+        return None
 
-def get_android_hwid():
-    m = str(uuid.uuid1())
-    return m
+# Get the Android device ID
+android_device_id = get_android_device_id()
 
-# Replace 'your_server_url' with the actual URL of your server
-site = requests.get('https://luxury-kleicha-549dac.netlify.app/hwid.txt')
-
-hardwareid = None  # Initialize the variable outside the if conditions
-
-try:
-    if platform.system().lower() == 'windows':
-        hardwareid = get_windows_hwid()
-        print('[INFO] Windows HWID:', hardwareid)
-    elif platform.system().lower() == 'android':
-        m = get_android_hwid()
-        print('[INFO] Android HWID:', m)
-
-    if (hardwareid and hardwareid in site.text) or (m and m in site.text):
-        print('[INFO] HWID found in database')
-    else:
-        print('[ERROR] HWID NOT in database')
-        time.sleep(5)
-        os._exit(0)  # Use 0 to indicate successful termination
-except Exception as e:
-    print('[ERROR] Exception:', str(e))
-    print('[ERROR] FAILED to connect to database')
-    time.sleep(5)
-    os._exit(1)  # Use 1 to indicate an error termination
+if android_device_id:
+    print(f'Android Device ID: {android_device_id}')
+else:
+    print('Failed to retrieve Android Device ID.')
