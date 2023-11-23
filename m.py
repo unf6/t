8b54e93fb9ed90a7374c3bbbd0f104c3caa8776d
Sadness <95174481+unf6@users.noreply.github.com>
2023-11-23 12:24:44 -0800

    Create m.py

---
+++ m.py
@@ -0,0 +1,24 @@
+import subprocess
+import requests
+import time
+import os
+import uuid
+
+# Get device UUID
+hardwareid = str(uuid.uuid1())
+
+# Replace 'your_android_server_url' with the actual URL of your server
+site = requests.get('your_android_server_url')
+
+try:
+    if hardwareid in site.text:
+        pass
+    else:
+        print('[ERROR] HWID NOT in database')
+        print('[HWID:' + hardwareid + ']')
+        time.sleep(5)
+        os._exit(0)  # Use 0 to indicate successful termination
+except:
+    print('[ERROR] FAILED to connect to database')
+    time.sleep(5)
+    os._exit(1)  # Use 1 to indicate an error termination
