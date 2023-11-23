c2b6dbf5dbbc477388abbb8d8a774e1d2e4edc36
Sadness <95174481+unf6@users.noreply.github.com>
2023-11-23 13:29:20 -0800

    Update m.py

--- m.py
+++ m.py
@@ -3,19 +3,56 @@ import requests
 import time
 import os
 import uuid
+import platform
 
-# Get device UUID
-hardwareid = str(uuid.uuid1())
+def get_windows_hwid():
+    hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
+    return hwid
 
-# Replace 'your_android_server_url' with the actual URL of your server
+def get_linux_hwid():
+    try:
+        # Run dmidecode and grep for UUID
+        output = subprocess.check_output(['dmidecode', '-s', 'system-uuid']).decode().strip()
+        return output
+    except subprocess.CalledProcessError:
+        print('[ERROR] Failed to get Linux HWID')
+        return None
+
+def get_mac_hwid():
+    try:
+        # Run system_profiler and grep for hardware UUID
+        output = subprocess.check_output(['system_profiler', 'SPHardwareDataType']).decode()
+        hwid_line = [line for line in output.splitlines() if 'Hardware UUID' in line][0]
+        _, hardwareid = hwid_line.split(':')
+        return hardwareid.strip()
+    except subprocess.CalledProcessError:
+        print('[ERROR] Failed to get macOS HWID')
+        return None
+
+def get_android_hwid():
+    return str(uuid.uuid1())
+
+# Replace 'your_server_url' with the actual URL of your server
 site = requests.get('https://luxury-kleicha-549dac.netlify.app/hwid.txt')
 
 try:
-    if hardwareid in site.text:
+    if platform.system().lower() == 'windows':
+        hardwareid = get_windows_hwid()
+    elif platform.system().lower() == 'linux':
+        hardwareid = get_linux_hwid()
+    elif platform.system().lower() == 'darwin':
+        hardwareid = get_mac_hwid()
+    elif platform.system().lower() == 'android':
+        hardwareid = get_android_hwid()
+    else:
+        print('[ERROR] Unsupported platform')
+        os._exit(1)
+
+    if hardwareid and hardwareid in site.text:
         pass
     else:
         print('[ERROR] HWID NOT in database')
-        print('[HWID:' + hardwareid + ']')
+        print('[HWID:' + str(hardwareid) + ']')
         time.sleep(5)
         os._exit(0)  # Use 0 to indicate successful termination
 except:
