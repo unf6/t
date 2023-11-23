a4c3aa7b3e990fc28c33d3a66a1b83a3e2b94c74
Sadness <95174481+unf6@users.noreply.github.com>
2023-11-23 13:38:18 -0800

    Update m.py

--- m.py
+++ m.py
@@ -30,7 +30,9 @@ def get_mac_hwid():
         return None
 
 def get_android_hwid():
-    return str(uuid.uuid1())
+    hwid = str(uuid.uuid1())
+    print('[INFO] Android HWID:', hwid)
+    return hwid
 
 # Replace 'your_server_url' with the actual URL of your server
 site = requests.get('https://luxury-kleicha-549dac.netlify.app/hwid.txt')
@@ -49,13 +51,14 @@ try:
         os._exit(1)
 
     if hardwareid and hardwareid in site.text:
-        pass
+        print('[INFO] HWID found in database')
     else:
         print('[ERROR] HWID NOT in database')
-        print('[HWID:' + str(hardwareid) + ']')
+        print('[HWID:', str(hardwareid) + ']')
         time.sleep(5)
         os._exit(0)  # Use 0 to indicate successful termination
-except:
+except Exception as e:
+    print('[ERROR] Exception:', str(e))
     print('[ERROR] FAILED to connect to database')
     time.sleep(5)
     os._exit(1)  # Use 1 to indicate an error termination
